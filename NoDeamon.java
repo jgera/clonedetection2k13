f6f0fe501f9f720ec519376904e86ed65fc40fe6
jacopo.zambon <jacopo.zambon@gmail.com>
2011-12-27 14:16:35 +0000

    salvataggio prima di provare un cambiamento radicale, perchè così non se ne esce! ~jz

--- NoDeamon.java
+++ NoDeamon.java
@@ -14,12 +14,14 @@ public class NoDeamon extends Thread{
 		System.out.println("DAEMON!");
 		boolean found=false;
 		while(!found){
-			int numDead=0;
+			/*int numDead=0;
 			for(int i=0; i<nodes.size();i++){
 				//System.out.println(nodes.get(i)+" "+nodes.get(i).getState());
-				if(nodes.get(i).getState()==State.WAITING || nodes.get(i).getState()==State.BLOCKED)
+				//if(nodes.get(i).getMessages().isEmpty())
+				if(nodes.get(i).getState()==State.BLOCKED || nodes.get(i).getState()==State.WAITING)
 					numDead++;
 			}
+			System.out.println("numDead è "+numDead);
 			if(numDead==nodes.size()){
 				System.out.println("Chiamiamo la merdaccia");
 				found=true;
@@ -27,7 +29,16 @@ public class NoDeamon extends Thread{
 				synchronized(parent){
 					parent.notify();
 				}
-			}
+			}*/
+			//System.out.println("Message proceeded "+Node.getmessagepro());
+			if(Node.getmessagepro()==0){ //all messages proceeded
+				System.out.println("Chiamiamo la merdaccia");
+				found=true;
+				parent.dimAlive();
+				synchronized(parent){
+					parent.notify();
+				}
+			}	
 		}
 	}
 }
